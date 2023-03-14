8e58881005fedf3fb75e4bcbd0d4ff2d8872dad7
matheuscara <matheus.dias.dev@gmail.com>
2023-03-13 23:51:57 -0300

    configurando .env

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,10 +3,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'Mat748596123',
-  database: 'estoquepro',
+  port: Number(process.env.PASSWORD_DATABASE),
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD_DATABASE,
+  database: process.env.DATABASE,
   entities: [
         "src/modules/*.entity.{ts,js}"
     ],
